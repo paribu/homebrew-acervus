54e4af21cb422dd6ac6a16037c5f12c500c55151
Blockchain Auteur <151908509+somestry@users.noreply.github.com>
2024-01-05 16:13:33 +0300

    Update acervus.rb

--- Formula/acervus/acervus.rb
+++ Formula/acervus/acervus.rb
@@ -2,7 +2,7 @@ class Acervus < Formula
   desc "CLI tool for interacting with Acervus Cloud"
   homepage "https://github.com/paribu/acervus-cli"
   url "https://github.com/paribu/acervus-cli/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "d02a6465aed995f4a18b289d7e16f203dd875cc2bd9db5ad2572bfd68754d175"
+  sha256 "8fd2e0066603f8afc983cf76176444a2212cfb107f5e6ec1f6eaa8c23fd5fad2"
   head "https://github.com/paribu/acervus-cli.git", branch: "main"
 
   depends_on "go@1.20" => :build
